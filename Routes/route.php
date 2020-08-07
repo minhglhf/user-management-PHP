32745cd77457cadb4dff5bfec319dd0e12567754
Minh Trần Quang <minhtq30102000@gmail.com>
2020-08-07 13:47:21 +0700

    router, redirect URL to get controller/action

---
+++ Routes/route.php
@@ -0,0 +1,5 @@
+<?php
+require_once "./app/Controllers/BaseController.php";
+require_once "./app/Models/BaseModel.php";
+require_once "./app/Configs/configs.php";
+require_once "./app/Core/App.php";
